9a619039047aa3996c59bf61a5e557554d0bd3a1
Jake Choi <jakecyg@gmail.com>
2023-06-10 01:18:32 -0500

    refactor start screen to stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/quiz.dart';
 import 'package:quiz_app/start_screen.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(colors: [
-                Color.fromARGB(255, 236, 109, 12),
-                Color.fromARGB(255, 236, 140, 66),
-              ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
-            ),
-            child: const StartScreen()),
-      ),
-    ),
-  );
+  runApp(const Quiz());
 }
